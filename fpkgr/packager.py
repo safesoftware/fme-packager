57b3929dcd6b8666e19d9a21a6a0103628cfb4bd
Jonathan Tang <jonathan.tang@safe.com>
2021-09-13 16:50:47 -0700

    reformat

--- fpkgr/packager.py
+++ fpkgr/packager.py
@@ -321,7 +321,9 @@ class FMEPackager:
 
         for expected_py_package in self.metadata.python_packages:
             lib_name = expected_py_package.name
-            if not any(wheel_name.startswith(lib_name) or wheel_name.startswith(lib_name.replace('-', '_')) for wheel_name in wheels):
+            if not any(
+                wheel_name.startswith(lib_name) or wheel_name.startswith(lib_name.replace('-', '_')) for wheel_name in
+                wheels):
                 raise ValueError("Python library '{}' is in metadata, but was not found".format(lib_name))
 
     def _copy_localization(self):
@@ -340,7 +342,7 @@ class FMEPackager:
         for name in os.listdir(src):
             path = os.path.join(src, name)
             if fnmatch(name, 'guiprompts_??.txt') or fnmatch(name, 'transformer-localized.??') or \
-                    any(fnmatch(name, glob) for glob in localized_doc_globs):
+                any(fnmatch(name, glob) for glob in localized_doc_globs):
                 if not os.path.exists(dest):
                     os.makedirs(dest)
                 print('Copying localization: ' + name)
