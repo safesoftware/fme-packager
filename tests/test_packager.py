b73142379f1aa881a0fb82914cf724dc598b6dae
Jonathan Tang <jonathan.tang@safe.com>
2021-09-15 09:49:31 -0700

    refactor getting fmx_path and reformat with black

--- tests/test_packager.py
+++ tests/test_packager.py
@@ -3,18 +3,30 @@ import os
 import pytest
 
 from fpkgr.exception import PythonCompatibilityError
-from fpkgr.packager import FMEPackager, check_fmx, is_valid_python_compatibility, check_custom_fmx
+from fpkgr.packager import (
+    FMEPackager,
+    check_fmx,
+    is_valid_python_compatibility,
+    check_custom_fmx,
+)
 
 
-@pytest.mark.parametrize("version, expected_validation", [
-    ("26", False),
-    ("2.6", False),
-    ("27", False),
-    ("ArcGISDesktop", False),
-    ("36", True),
-    ("37", True),
-    ("3.8.9", True),
-])
+def get_fmx_path(transformer_dir, transformer_name):
+    return os.path.join(transformer_dir, "{}.fmx".format(transformer_name))
+
+
+@pytest.mark.parametrize(
+    "version, expected_validation",
+    [
+        ("26", False),
+        ("2.6", False),
+        ("27", False),
+        ("ArcGISDesktop", False),
+        ("36", True),
+        ("37", True),
+        ("3.8.9", True),
+    ],
+)
 def test_is_valid_python_compatibility(version, expected_validation):
     assert is_valid_python_compatibility(version) == expected_validation
 
@@ -22,17 +34,18 @@ def test_is_valid_python_compatibility(version, expected_validation):
 def test_check_fmx(valid_package_dir):
     packager = FMEPackager(valid_package_dir)
     for transformer in packager.metadata.transformers:
-        src = os.path.join(valid_package_dir, 'transformers')
-        fmx_path = os.path.join(src, "{}.fmx".format(transformer.name))
+        fmx_path = get_fmx_path(
+            os.path.join(valid_package_dir, "transformers"), transformer.name
+        )
         check_fmx(packager.metadata, transformer, fmx_path)
 
 
 def test_check_fmx_with_compatibility_error(incompatible_package_dir):
     packager = FMEPackager(incompatible_package_dir)
     for transformer in packager.metadata.transformers:
-        src = os.path.join(incompatible_package_dir, 'transformers')
-        fmx_path = os.path.join(src, "{}.fmx".format(transformer.name))
-
+        fmx_path = get_fmx_path(
+            os.path.join(incompatible_package_dir, "transformers"), transformer.name
+        )
         with pytest.raises(PythonCompatibilityError):
             check_fmx(packager.metadata, transformer, fmx_path)
 
@@ -40,16 +53,18 @@ def test_check_fmx_with_compatibility_error(incompatible_package_dir):
 def test_check_custom_fmx(custom_package_dir):
     packager = FMEPackager(custom_package_dir)
     for transformer in packager.metadata.transformers:
-        src = os.path.join(custom_package_dir, 'transformers')
-        fmx_path = os.path.join(src, "{}.fmx".format(transformer.name))
+        fmx_path = get_fmx_path(
+            os.path.join(custom_package_dir, "transformers"), transformer.name
+        )
         check_custom_fmx(packager.metadata, transformer, fmx_path)
 
 
 def test_check_custom_fmx_with_error(incompatible_custom_package_dir):
     packager = FMEPackager(incompatible_custom_package_dir)
     for transformer in packager.metadata.transformers:
-        src = os.path.join(incompatible_custom_package_dir, 'transformers')
-        fmx_path = os.path.join(src, "{}.fmx".format(transformer.name))
-
+        fmx_path = get_fmx_path(
+            os.path.join(incompatible_custom_package_dir, "transformers"),
+            transformer.name,
+        )
         with pytest.raises(PythonCompatibilityError):
             check_custom_fmx(packager.metadata, transformer, fmx_path)
